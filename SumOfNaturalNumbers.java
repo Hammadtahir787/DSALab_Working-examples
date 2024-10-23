f308660274adc38b7999238dfcc88bafe8de8f3d
Hammadtahir787 <hammadtahir787@gmail.com>
2024-10-22 21:40:01 -0700

    Add files via upload

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,13 @@
+
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+      int sum = 0;
+      for (int i = 1; i<=10; i++ ){
+          sum += i;
+          
+      }
+      System.out.println("The sum of first ten natural numbers are :" + sum);
+      
+    }
+}
